cc26b47e81545bb0113539f0462b73c220f24c0d
Sakari Marttinen <sakkenino@gmail.com>
2023-07-11 10:35:14 +0000

    Add 1.0.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# Iorch
+
+## Building
+see https://wiki.archlinux.org/title/archiso#Build_the_ISO
---
+++ iorch/profiledef.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="iorch"
+iso_label="IORCH_1.0.0)"
+iso_publisher="Sakari Marttinen <sakari.marttinen@helsinki.fi>"
+iso_application="Iorch"
+iso_version="1.0.0"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+)
